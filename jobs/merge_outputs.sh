0cf0aafd6298d7dc066ddcc80842de0b9c357bff
Krishan Mistry <kvjmistry@gmail.com>
2024-07-26 00:27:40 -0500

    remove kinematics python file

--- jobs/merge_outputs.sh
+++ jobs/merge_outputs.sh
@@ -2,8 +2,8 @@
 Model="Leptoquark_SM"
 mkdir -p merged
 
-for f in $(ls ${Model}_nexus); do cat ${Model}_nexus/$f >> merged/${Model}_nexus_reco_merged.txt;done
-for f in $(ls ${Model}_1mm_smear); do cat ${Model}_1mm_smear/$f >> merged/${Model}_1mm_smear_reco_merged.txt;done
-for f in $(ls ${Model}_2mm_smear); do cat ${Model}_2mm_smear/$f >> merged/${Model}_2mm_smear_reco_merged.txt;done
-for f in $(ls ${Model}_4mm_smear); do cat ${Model}_4mm_smear/$f >> merged/${Model}_4mm_smear_reco_merged.txt;done
-for f in $(ls ${Model}_10mm_smear); do cat ${Model}_10mm_smear/$f >> merged/${Model}_10mm_smear_reco_merged.txt;done
+for f in $(ls ${Model}_nexus | grep *.txt); do cat ${Model}_nexus/$f >> merged/${Model}_nexus_reco_merged.txt;done
+for f in $(ls ${Model}_1mm_smear | grep *.txt); do cat ${Model}_1mm_smear/$f >> merged/${Model}_1mm_smear_reco_merged.txt;done
+for f in $(ls ${Model}_2mm_smear | grep *.txt); do cat ${Model}_2mm_smear/$f >> merged/${Model}_2mm_smear_reco_merged.txt;done
+for f in $(ls ${Model}_4mm_smear | grep *.txt); do cat ${Model}_4mm_smear/$f >> merged/${Model}_4mm_smear_reco_merged.txt;done
+for f in $(ls ${Model}_10mm_smear | grep *.txt); do cat ${Model}_10mm_smear/$f >> merged/${Model}_10mm_smear_reco_merged.txt;done
